feb15c5dc31260e1a78c2f92e21f9db5807b30c2
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2023-12-09 16:34:06 +0100

    Update files-list.c
    Added function add_file_entry

--- files-list.c
+++ files-list.c
@@ -12,3 +12,50 @@ void clear_files_list(files_list_t *list) {
         free(tmp);
     }
 }
+
+files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
+    files_list_entry_t *new_entry = malloc(sizeof(files_list_entry_t));
+    if (new_entry == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    strncpy(new_entry->path_and_name, file_path, sizeof(new_entry->path_and_name));
+    new_entry->prev = NULL;
+    new_entry->next = NULL;
+
+    files_list_entry_t *temp = list->head;
+
+    while (temp != NULL) {
+        if (strcmp(temp->path_and_name, file_path) == 0) {
+            free(new_entry);
+            return NULL;
+        } else if (strcmp(temp->path_and_name, file_path) > 0) {
+            // Insert before current entry
+            new_entry->prev = temp->prev;
+            new_entry->next = temp;
+
+            if (temp->prev != NULL) {
+                temp->prev->next = new_entry;
+            } else {
+                list->head = new_entry;
+            }
+
+            temp->prev = new_entry;
+            return new_entry;
+        }
+
+        temp = temp->next;
+    }
+
+    if (list->head == NULL) {
+        list->head = new_entry;
+        list->tail = new_entry;
+    } else {
+        new_entry->prev = list->tail;
+        list->tail->next = new_entry;
+        list->tail = new_entry;
+    }
+
+    return new_entry;
+}
