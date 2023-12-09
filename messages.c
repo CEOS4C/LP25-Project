46dcaa5132e567ec433612a76a166c05b5366542
Moheeel <116997327+Moheeel@users.noreply.github.com>
2023-12-09 16:36:43 +0100

    Create messages.c

---
+++ messages.c
@@ -0,0 +1,37 @@
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "messages.h"
+
+int send_file_entry(int msg_queue, int recipient, files_list_entry_t *file_entry, int cmd_code) {
+    int result;
+    any_message_t message;
+
+    message.list_entry.op_code = cmd_code;
+    message.list_entry.payload = *file_entry;
+
+    result = msgsnd(msg_queue, &message, sizeof(files_list_entry_transmit_t), 0);
+    if (result == -1) {
+        perror("msgsnd");
+    }
+
+    return result;
+}
+
+int send_analyze_dir_command(int msg_queue, int recipient, char *target_dir) {
+    int result;
+    any_message_t message;
+
+    message.analyze_dir_command.mtype = recipient;
+    message.analyze_dir_command.op_code = COMMAND_CODE_ANALYZE_DIR;
+    strncpy(message.analyze_dir_command.target, target_dir, PATH_SIZE - 1);
+    message.analyze_dir_command.target[PATH_SIZE - 1] = '\0';
+
+    result = msgsnd(msg_queue, &message, sizeof(analyze_dir_command_t), 0);
+    if (result == -1) {
+        perror("msgsnd");
+    }
+
+    return result;
+}
