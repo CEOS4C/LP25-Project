7727e7f047f8daa12d8f7461c2fb0f064a5621da
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2023-12-09 16:38:46 +0100

    Update messages.c
    Added functions send_list_end, send_terminate_command & send_terminate_confirm

--- messages.c
+++ messages.c
@@ -47,3 +47,48 @@ int send_analyze_file_response(int msg_queue, int recipient, files_list_entry_t 
 int send_files_list_element(int msg_queue, int recipient, files_list_entry_t *file_entry) {
     return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_FILE_ENTRY);
 }
+
+int send_list_end(int msg_queue, int recipient) {
+    int result;
+    any_message_t message;
+
+    message.simple_command.mtype = recipient;
+    message.simple_command.message = COMMAND_CODE_LIST_COMPLETE;
+
+    result = msgsnd(msg_queue, &message, sizeof(simple_command_t), 0);
+    if (result == -1) {
+        perror("msgsnd");
+    }
+
+    return result;
+}
+
+int send_terminate_command(int msg_queue, int recipient) {
+    int result;
+    any_message_t message;
+
+    message.simple_command.mtype = recipient;
+    message.simple_command.message = COMMAND_CODE_TERMINATE;
+
+    result = msgsnd(msg_queue, &message, sizeof(simple_command_t), 0);
+    if (result == -1) {
+        perror("msgsnd");
+    }
+
+    return result;
+}
+
+int send_terminate_confirm(int msg_queue, int recipient) {
+    int result;
+    any_message_t message;
+
+    message.simple_command.mtype = recipient;
+    message.simple_command.message = COMMAND_CODE_TERMINATE_OK;
+
+    result = msgsnd(msg_queue, &message, sizeof(simple_command_t), 0);
+    if (result == -1) {
+        perror("msgsnd");
+    }
+
+    return result;
+}
