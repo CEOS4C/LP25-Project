73908692b0132b3b97150ee1ce0f9495758a38dd
Moheeel <116997327+Moheeel@users.noreply.github.com>
2024-01-07 18:15:54 +0100

    Update file-properties.c

--- file-properties.c
+++ file-properties.c
@@ -1,16 +1,29 @@
 #include <sys/stat.h>
-#include <dirent.h>
 #include <openssl/evp.h>
 #include <unistd.h>
-#include <assert.h>
 #include <string.h>
 #include <fcntl.h>
 #include <stdio.h>
 
-#include "utility.h"
 #include "file-properties.h"
 #include "defines.h"
 
+
+/*!
+ * @brief get_file_stats gets all of the required information for a file (inc. directories)
+ * @param the files list entry
+ * You must get:
+ * - for files:
+ *   - mode (permissions)
+ *   - mtime (in nanoseconds)
+ *   - size
+ *   - entry type (FICHIER)
+ *   - MD5 sum
+ * - for directories:
+ *   - mode
+ *   - entry type (DOSSIER)
+ * @return -1 in case of error, 0 else
+ */
 int get_file_stats(files_list_entry_t *entry) {
     if (entry == NULL) {
         return -1;
@@ -22,8 +35,11 @@ int get_file_stats(files_list_entry_t *entry) {
         return -1;
     }
 
+    entry->mtime.tv_sec = file_stat.st_mtime;
+    entry->mtime.tv_nsec = 0;
+
+    entry->size = file_stat.st_size;
     entry->mode = file_stat.st_mode;
-    entry->mtime = file_stat.st_mtim;
 
     if (S_ISDIR(file_stat.st_mode)) {
         entry->entry_type = DOSSIER;
@@ -42,7 +58,18 @@ int get_file_stats(files_list_entry_t *entry) {
     return 0;
 }
 
+
+/*!
+ * @brief compute_file_md5 computes a file's MD5 sum
+ * @param the pointer to the files list entry
+ * @return -1 in case of error, 0 else
+ * Use libcrypto functions from openssl/evp.h
+ */
 int compute_file_md5(files_list_entry_t *entry) {
+    if (entry == NULL || entry->path_and_name == NULL) {
+        fprintf(stderr, "Invalid entry or path.\n");
+        return -1;
+    }
     FILE *file = fopen(entry->path_and_name, "rb");
     if (!file) {
         perror("fopen");
@@ -84,15 +111,43 @@ int compute_file_md5(files_list_entry_t *entry) {
     return 0;
 }
 
+
+/*!
+ * @brief directory_exists tests the existence of a directory
+ * @path_to_dir a string with the path to the directory
+ * @return true if directory exists, false else
+ */
 bool directory_exists(char *path_to_dir) {
+    if (path_to_dir == NULL || *path_to_dir == '\0') {
+        fprintf(stderr, "Invalid path to directory.\n");
+        return false;
+    }
     struct stat dir_stat;
     return stat(path_to_dir, &dir_stat) == 0 && S_ISDIR(dir_stat.st_mode);
 }
 
+
+/*!
+ * @brief is_directory_writable tests if a directory is writable
+ * @param path_to_dir the path to the directory to test
+ * @return true if dir is writable, false else
+ * Hint: try to open a file in write mode in the target directory.
+ */
 bool is_directory_writable(char *path_to_dir) {
-    int fd = open(path_to_dir, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (path_to_dir == NULL || *path_to_dir == '\0') {
+        fprintf(stderr, "Invalid path to directory.\n");
+        return false;
+    }
+
+    char temp_file_path[PATH_SIZE];
+    snprintf(temp_file_path, sizeof(temp_file_path), "%s/temp_file_for_write_test", path_to_dir);
+
+    int fd = open(temp_file_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd != -1) {
         close(fd);
+
+        unlink(temp_file_path);
+
         return true;
     }
 
